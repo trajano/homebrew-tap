23b0a17bedaedf6178481a88f3f352fbd4216d9c
trajano <110627+trajano@users.noreply.github.com>
2026-02-28 02:16:32 +0000

    feat: bump aliae formula to v1.15.0

--- Formula/aliae.rb
+++ Formula/aliae.rb
@@ -2,33 +2,33 @@ class Aliae < Formula
   desc "Cross shell and platform alias management"
   homepage "https://aliae.dev"
   license "MIT"
-  version "1.14.0"
+  version "1.15.0"
   head "https://github.com/trajano/aliae.git", branch: "master"
   depends_on "go" => :build
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/trajano/aliae/releases/download/v1.14.0/aliae-darwin-arm64"
-      sha256 "532467b70965ea26052b15fa2462a4d2b86199566c4a0de13b129dc3ad900994"
+      url "https://github.com/trajano/aliae/releases/download/v1.15.0/aliae-darwin-arm64"
+      sha256 "21dc865977741bed99ab727026d5e1d75544b9b2d5273b6f3e06836b585629c4"
     else
-      url "https://github.com/trajano/aliae/releases/download/v1.14.0/aliae-darwin-amd64"
-      sha256 "f8e9dfc4e80ecc179c5acf03cfcdd1872cc7bfa1897af51a164f34d35f4aa7b8"
+      url "https://github.com/trajano/aliae/releases/download/v1.15.0/aliae-darwin-amd64"
+      sha256 "b2a7dd335759e93a3d4e0216880dd3eb60f127a3a6d9cdd9eed85edc9854f9b3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/trajano/aliae/releases/download/v1.14.0/aliae-linux-arm64"
-      sha256 "4536799792bf0c27132fa7b303932ed494ab88d85f1e36e1e7fae3ce4a858cd1"
+      url "https://github.com/trajano/aliae/releases/download/v1.15.0/aliae-linux-arm64"
+      sha256 "1865260d76a09e802bfe7c003743a9e10256b2cda0c2c7846dd0f1fc043de6c0"
     else
-      url "https://github.com/trajano/aliae/releases/download/v1.14.0/aliae-linux-amd64"
-      sha256 "c4de50c1192575f14ee9e2af77dbbbc31cc23764cc7446805b3175c577d3aead"
+      url "https://github.com/trajano/aliae/releases/download/v1.15.0/aliae-linux-amd64"
+      sha256 "b63f71789705990ad5b5648963a4cb9f5ec5d6b6d60185ded135e0f251bfce40"
     end
   end
 
   resource "source" do
-    url "https://github.com/trajano/aliae/archive/refs/tags/v1.14.0.tar.gz"
-    sha256 "320988c89504fcdf191e98bfb20ad48958bb38a0a3b730da837bd1836837d267"
+    url "https://github.com/trajano/aliae/archive/refs/tags/v1.15.0.tar.gz"
+    sha256 "0909fef7ad68c9fecb1d2a51297813813230147b45e05edb24938732fe8620ea"
   end
 
   def install
